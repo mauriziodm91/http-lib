8d623409740e550f86649b36f8fcd2f1c6a9651c
maurizio <mauriziodm91@gmail.com>
2021-08-25 00:00:17 -0300

    initial commit

---
+++ easyhttp.js
@@ -0,0 +1,28 @@
+function easyHTTP() {
+    this.http = new XMLHttpRequest();
+
+}
+
+//GET
+easyHTTP.prototype.get = function (url, callback){
+    this.http.open('GET', url, true);
+    
+    let self = this;
+    this.http.onload = function (){
+        if(self.http.status === 200){
+           callback(null, self.http.responseText); 
+        } else{
+            callback('ERROR :' + self.http.status);
+        }
+    }
+
+    this.http.send();
+}
+
+
+//POST
+
+
+//PUT
+
+//DELETE
